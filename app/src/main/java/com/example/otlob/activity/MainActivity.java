ffafc05c310adabfe12991bad600cda2aa6ad5bb
ahmed <ahmedbahig77@gmail.com>
2020-10-22 23:19:29 +0200

    Commit

---
+++ app/src/main/java/com/example/otlob/activity/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.otlob.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.otlob.R;
+import com.example.otlob.databinding.ActivityMainBinding;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this , R.layout.activity_main);
+
+        binding.btnSignIn.setOnClickListener(this);
+        binding.btnRegister.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_register :
+                startActivity(new Intent(com.example.otlob.activity.MainActivity.this , Register.class));
+                break;
+
+            case R.id.btn_sign_in :
+                startActivity(new Intent(com.example.otlob.activity.MainActivity.this , Login.class));
+                break;
+        }
+    }
+}
